934b7ad884ab72a581d6f43e15896b6b3f49c4e0
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-08-12 07:27:21 +0200

    Get current formatted date on button and date picker header

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+import { format, fromUnixTime, getUnixTime } from "date-fns";
+
 // -- GLOBAL --
 const datePickerButton = document.querySelector(".date-picker-button");
 const datePicker = document.querySelector(".date-picker");
+const datePickerHeaderText = document.querySelector(".current-month");
 
-// -- TOGGLE DATE PICKER
+// -- TOGGLE DATE PICKER --
 datePickerButton.addEventListener("click", () => {
   datePicker.classList.toggle("show");
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+  setUpDatePicker(selectedDate);
 });
+
+// -- SET AND FORMAT DATES ON DATE PICKER BUTTON --
+function setDate(date) {
+  datePickerButton.textContent = format(date, "MMMM do, yyyy");
+  datePickerButton.dataset.selectedDate = getUnixTime(date);
+}
+
+setDate(new Date());
+
+// -- SET AND FORMAT DATES ON DATE PICKER --
+function setUpDatePicker(selectedDate) {
+  datePickerHeaderText.textContent = format(selectedDate, "MMMM - yyyy");
+}
