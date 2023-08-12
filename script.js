942d0165ef97316bca92bc3b05d276c356180a71
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-08-12 18:02:34 +0200

    Set up dates in date picker

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 import {
   addMonths,
+  eachDayOfInterval,
+  endOfMonth,
+  endOfWeek,
   format,
   fromUnixTime,
   getUnixTime,
+  isSameDay,
+  isSameMonth,
+  startOfMonth,
+  startOfWeek,
   subMonths,
 } from "date-fns";
 
@@ -12,6 +19,7 @@ const datePicker = document.querySelector(".date-picker");
 const datePickerHeaderText = document.querySelector(".current-month");
 const previousMonthButton = document.querySelector(".prev-month-button");
 const nextMonthButton = document.querySelector(".next-month-button");
+const dateGrid = document.querySelector(".date-picker-grid-dates");
 let currentDate = new Date();
 
 // -- TOGGLE DATE PICKER --
@@ -31,17 +39,45 @@ function setDate(date) {
 setDate(new Date());
 
 // -- SET AND FORMAT DATES ON DATE PICKER --
-function setUpDatePicker() {
+function setUpDatePicker(selectedDate) {
   datePickerHeaderText.textContent = format(currentDate, "MMMM - yyyy");
+  setupDates(selectedDate);
 }
 
 // -- SET UP ARROW BUTTONS ON DATE PICKER
 nextMonthButton.addEventListener("click", () => {
   currentDate = addMonths(currentDate, 1);
-  setUpDatePicker();
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+  setUpDatePicker(selectedDate);
 });
 
 previousMonthButton.addEventListener("click", () => {
   currentDate = subMonths(currentDate, 1);
-  setUpDatePicker();
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+  setUpDatePicker(selectedDate);
 });
+
+function setupDates(selectedDate) {
+  const firstWeekStart = startOfWeek(startOfMonth(currentDate));
+  const lastWeekEnd = endOfWeek(endOfMonth(currentDate));
+  const dates = eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd });
+  dateGrid.innerHTML = "";
+  dates.forEach((date) => {
+    const dateElement = document.createElement("button");
+    dateElement.classList.add("date");
+    dateElement.textContent = date.getDate();
+    if (!isSameMonth(date, currentDate)) {
+      dateElement.classList.add("date-picker-other-month-date");
+    }
+    if (isSameDay(date, selectedDate)) {
+      dateElement.classList.add("selected");
+    }
+
+    dateElement.addEventListener("click", () => {
+      setDate(date);
+      datePicker.classList.remove("show");
+    });
+
+    dateGrid.appendChild(dateElement);
+  });
+}
